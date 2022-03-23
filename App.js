9f72157c9694da379bbccaa9ff34db0ab41d0439
Valentin Georgiev <v.georgiev@up2technology.com>
2022-03-23 17:09:29 +0200

    feat: request location permission

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/split-platform-components */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,15 +7,50 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import type {Node} from 'react';
 import {View} from 'react-native';
+import {PermissionsAndroid, Platform} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
 // import HomeScreen from './src/screens/HomeScreen';
 import DestinationSearch from './src/screens/DestinationSearch';
 // import SearchResult from './src/screens/SearchResult';
 
 const App: () => Node = () => {
+  const androidPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Uber App Camera Permission',
+          message:
+            'Uber App needs access to your location ' +
+            'so you can take awesome rides.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the location');
+      } else {
+        console.log('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      androidPermission();
+    } else {
+      // IOS
+      Geolocation.requestAuthorization();
+    }
+  }, []);
+
   return (
     <View>
       <DestinationSearch />
